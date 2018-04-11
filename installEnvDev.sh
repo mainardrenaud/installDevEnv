40b50a6727642a677b054d4ec819af81074c98c0
rmainard <rmainard@efalia.com>
2018-04-11 16:00:39 +0200

    maj

--- installEnvDev.sh
+++ installEnvDev.sh
@@ -35,8 +35,8 @@ sudo apt-get install php7.2 php7.2-fpm php7.2-mysql php7.2-curl php7.2-xml php7.
 sudo service php7.2-fpm start
 sudo service nginx start
 #création du repertoire de dev passer en parametre dans multigest-back
-sudo mkdir /mnt/e/developpements/multigest-back;
-cd /mnt/e/developpements/multigest-back
+sudo mkdir /var/ww/html/multigest-back;
+cd /var/ww/html/multigest-back
 sudo wget https://raw.githubusercontent.com/mainardrenaud/installDevEnv/master/infos.php
 curl -Is http://localhost :8081 
 #installation mariadb
@@ -46,7 +46,7 @@ sudo apt-get install mariadb*
 sudo service mysql start
 sudo mysql_secure_installation
 #installation de composer
-cd /mnt/e/developpements/
+cd /var/ww/html/
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php
@@ -55,7 +55,7 @@ sudo mv composer.phar composer
 #installation de git pour install de sf4
 sudo apt-get install git
 #installation symfony4
-cd /mnt/e/developpements
+cd /var/ww/html/
 sudo ./composer create-project symfony/skeleton multigest-back
 sudo mkdir multigest-back/logs
 
